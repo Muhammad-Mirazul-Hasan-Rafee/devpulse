1888cb04f03c0ae9227d5e48a19c7ed0ceb99809
Hasan Rafee <rafeehasan88@gmail.com>
2026-08-19 18:10:52 +0600

    table created

--- src/DB/index.ts
+++ src/DB/index.ts
@@ -1,15 +1,39 @@
-import config from "../config";
 import { Pool } from "pg";
-export const pool = new Pool({
-    connectionString: config.CONNECTIVITYSTR,
-});
+import config from "../config";
 
-export const initDB = async() =>{
+export const pool = new Pool({ connectionString: config.CONNECTIVITYSTR });
+
+export const initDB = async () => {
     try {
-        
-    } catch (error:any) {
-        console.error("Error creating tables:");
-    console.error(error.message);
-    console.error(error);
+        await pool.query(`
+            CREATE TABLE IF NOT EXISTS 
+            users(
+            id SERIAL PRIMARY KEY,
+            name VARCHAR(30),
+            email VARCHAR(25) UNIQUE NOT NULL,
+            password TEXT NOT NULL,
+            role VARCHAR(20) DEFAULT 'contributor',
+            created_at TIMESTAMP DEFAULT NOW(),
+            updated_at TIMESTAMP DEFAULT NOW()    )
+            `);
+
+            await pool.query(`
+                CREATE TABLE IF NOT EXISTS issues(
+                id SERIAL PRIMARY KEY,
+                title VARCHAR(150),
+                description TEXT,
+
+                type VARCHAR(50) NOT NULL CHECK (type IN ('bug', 'feature_request')),
+
+
+                status VARCHAR(50) NOT NULL DEFAULT 'open' CHECK (status IN('open', 'in_progress', 'resolved')),
+
+                reporter_id INTEGER,
+
+                created_at TIMESTAMP DEFAULT NOW(),
+                updated_at TIMESTAMP DEFAULT NOW())
+                `);
+    } catch (error) {
+
     }
 };

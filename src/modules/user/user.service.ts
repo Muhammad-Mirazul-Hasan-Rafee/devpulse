2be0fd0865aed145e70baeb5d6e14f34c869616e
Rafee <rafeehasan88@gmail.com>
2026-09-05 21:39:00 +0600

    added createUser

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,8 +1,18 @@
 import bcrypt from "bcryptjs";
 import type { IUser } from "./user.interface";
+import { pool } from "../../DB";
 
 //   create user
 const createUserIntoDb = async(payLoad: IUser)=>{
     const {name, email, password, role} = payLoad;
-    const hashpassword = await bcrypt
+    const hashpassword = await bcrypt.hash(password,10);
+
+    // if(role){
+
+    // }
+
+    const result = await pool.query(`
+        
+    `);
+
 };
